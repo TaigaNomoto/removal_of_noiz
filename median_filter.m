cff3ee19c938481587e52ea871d5fc287b82236d
Taiga Nomoto <taiga12257@gmail.com>
2019-09-24 15:09:25 +0900

    first commit

---
+++ median_filter.m
@@ -0,0 +1,17 @@
+clear();
+
+files=dir();
+f=csvread('../DATA/20190924_140835.csv');
+
+fx=f(:,2);
+fy=f(:,1);
+
+y=medfilt1(fy, 10);
+
+plot(fx, fy, fx, y);
+legend('Original', 'Filtered');
+legend('boxoff');
+xlabel('åoâﬂéûä‘(s)');
+ylabel('êSîèä‘äu(s)');
+title('20190924_140835.csv','FontSize',12);
+ 
